718f6460a0d55d218daa5c20385ca6e4ec28dcbf
krendeeel <79937554+krendeeel@users.noreply.github.com>
2025-01-08 02:16:58 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
